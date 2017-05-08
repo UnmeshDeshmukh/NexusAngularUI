76c910746567584583d8afb5ab87eb2f49e63be1
Aniket Khaire <aniketd.khaire@gmail.com>
2017-05-07 18:27:22 -0700

    Updated code

--- UI with angular/modules/adm-department/controller.js
+++ UI with angular/modules/adm-department/controller.js
@@ -5,70 +5,110 @@ angular.module('AdminDepartment')
 ['$scope','$rootScope','$cookies','$location','AdminDepartmentService','AuthenticationService',
 function($scope,$rootScope,$cookies,$location,AdminDepartmentService,AuthenticationService){
   //AuthenticationService.ClearCredentials();
-  var currentUserData = $cookies.get('globals');
-  var token = $cookies.get('token');
-  var org_name = $cookies.get('orgname');
-  var currentUserEmail = $cookies.get('email');
-  console.log(token);
-  var currentDeptHead = null;
-  $scope.showDeptHead = false;
-  $scope.showDeptDesc = false;
+    var currentUserData = $cookies.get('globals');
+    var token = $cookies.get('token');
+    var org_name = $cookies.get('orgname');
+    var currentUserEmail = $cookies.get('email');
+    console.log(token);
+    var currentDeptHead = null;
+    $scope.showDeptHead = false;
+    $scope.showDeptDesc = false;
 
-// $scope.updateDepartment = function(){
-//   AdminDepartmentService.updateDepartmentDetails($scope.dept_name,$scope.dept_desc,$scope.org_name,$scope.dept_head,token, function(response,data){
-//       //TODO TRACK THE ERRORS IN ADMIN DEPARTMENT
-//
-//     }
-// }
+    // $scope.updateDepartment = function(){
+    //   AdminDepartmentService.updateDepartmentDetails($scope.dept_name,$scope.dept_desc,$scope.org_name,$scope.dept_head,token, function(response,data){
+    //       //TODO TRACK THE ERRORS IN ADMIN DEPARTMENT
+    //
+    //     }
+    // }
 
-//alert(token);
-var init = function () {
-   AdminDepartmentService.getAllDepartments(org_name,token,function(response,data){
-         //TODO Track the errors
-         $scope.myData = data;
+    //alert(token);
+    var init = function () {
+        AdminDepartmentService.getAllDepartments(org_name,token,function(response,data){
+            //TODO Track the errors
+            $scope.myData = data;
 
-         AdminDepartmentService.getAllUsers(currentUserEmail,token,function(response,data){
-            $scope.allusers = data;
+            AdminDepartmentService.getAllUsers(currentUserEmail,token,function(response,data){
+                $scope.allusers = data;
+            });
+        })
+    };
 
+    init();
 
+    $scope.getDeptDetails = function($event) {
+        AdminDepartmentService.getDetails($scope.updatedept_name,token,function(response,data){
+            $scope.showDeptHead = true;
+            $scope.showDeptDesc = true;
+            $scope.currentDeptData = data;
 
-         });
-   })
-};
-init();
+        })
+    }
 
+    $scope.addDepartment = function(){
+        AdminDepartmentService.postDepartment($scope.dept_name,$scope.dept_desc,$scope.dept_head,token, function(response,data){
+            console.log("Admin-Department-Controller: "+ response);
 
-$scope.getDeptDetails = function($event) {
+            //notification
+            if( data.message === 'Successful'){
+                $.notify({
+                    icon: "pe-7s-gift",
+                    message: "Department was added successfully!"
+                },{
+                    type: 'info',
+                    timer: 4000,
+                    placement: {
+                        from: 'top',
+                        align: 'center'
+                    }
+                });
+            } else {
+                $.notify({
+                    icon: "pe-7s-gift",
+                    message: "Snap! Something went wrong while adding the department!"
+                },{
+                    type: 'danger',
+                    timer: 4000,
+                    placement: {
+                        from: 'top',
+                        align: 'center'
+                    }
+                });
+            }
+        });
+    };
 
-  AdminDepartmentService.getDetails($scope.updatedept_name,token,function(response,data){
-    $scope.showDeptHead = true;
-    $scope.showDeptDesc = true;
-    $scope.currentDeptData = data;
+    $scope.updateDepartment = function(){
+        AdminDepartmentService.postDepartment($scope.updatedept_name,$scope.updatedept_desc,$scope.updatedept_head,token, function(response,data){
+            console.log(response);
+            if( data.message === 'Successful'){
+                $.notify({
+                    icon: "pe-7s-gift",
+                    message: "Department was updated successfully!"
+                },{
+                    type: 'info',
+                    timer: 4000,
+                    placement: {
+                        from: 'top',
+                        align: 'center'
+                    }
+                });
+            } else {
+                $.notify({
+                    icon: "pe-7s-gift",
+                    message: "Snap! Something went wrong while updating the department!"
+                },{
+                    type: 'danger',
+                    timer: 4000,
+                    placement: {
+                        from: 'top',
+                        align: 'center'
+                    }
+                });
+            }
+        });
+    };
 
-    // 
-    // AdminDepartmentService.postDepartment($scope.updatedept_name,$scope.updatedept_desc,$scope.updatedept_head,token, function(response,data){
-    //   });
-
-  })
-}
-
-$scope.addDepartment = function(){
-
-    AdminDepartmentService.postDepartment($scope.dept_name,$scope.dept_desc,$scope.dept_head,token, function(response,data){
-
-      });
-};
-
-
-$scope.updateDepartment = function(){
-
-    AdminDepartmentService.postDepartment($scope.updatedept_name,$scope.updatedept_desc,$scope.updatedept_head,token, function(response,data){
-
-      });
-};
-
-$scope.signout = function() {
-    AuthenticationService.ClearCredentials();
-}
-
+    $scope.signout = function() {
+        AuthenticationService.ClearCredentials();
+    }
 }]);
